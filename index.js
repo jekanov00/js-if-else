8d6f84e168f9c9fa78609de3a68a19b6981f627e
Eugene Samoylenko <evgeniysamoylenko2000@gmail.com>
2020-06-23 12:50:32 +0300

    Add first part of verifications

---
+++ index.js
@@ -0,0 +1,122 @@
+// First part
+console.log('First part:')
+
+console.log('isZero(1) = ', isZero(1));
+console.log('isZero(0) = ', isZero(0));
+console.log('isZero(-3) = ', isZero(-3));
+
+console.log('');
+
+console.log('isOverZero(1) = ', isOverZero(1));
+console.log('isOverZero(0) = ', isOverZero(0));
+console.log('isOverZero(-3) = ', isOverZero(-3));
+
+console.log('');
+
+console.log('isBelowZero(1) = ', isBelowZero(1));
+console.log('isBelowZero(0) = ', isBelowZero(0));
+console.log('isBelowZero(-3) = ', isBelowZero(-3));
+
+console.log('');
+
+console.log('isOverEqualZero(1) = ', isOverEqualZero(1));
+console.log('isOverEqualZero(0) = ', isOverEqualZero(0));
+console.log('isOverEqualZero(-3) = ', isOverEqualZero(-3));
+
+console.log('');
+
+console.log('isBelowEqualZero(1) = ', isBelowEqualZero(1));
+console.log('isBelowEqualZero(0) = ', isBelowEqualZero(0));
+console.log('isBelowEqualZero(-3) = ', isBelowEqualZero(-3));
+
+console.log('');
+
+console.log('isNotZero(1) = ', isNotZero(1));
+console.log('isNotZero(0) = ', isNotZero(0));
+console.log('isNotZero(-3) = ', isNotZero(-3));
+
+console.log('');
+
+console.log('isTest(1) = ', isTest('test'));
+console.log('isTest(0) = ', isTest('тест'));
+console.log('isTest(-3) = ', isTest(3));
+
+console.log('');
+
+console.log('isStringOne(1) = ', isStringOne('1'));
+console.log('isStringOne(0) = ', isStringOne(1));
+console.log('isStringOne(-3) = ', isStringOne(3));
+
+console.log('');
+
+/**
+ *
+ * @param {number} a
+ * @returns {boolean}
+ */
+function isZero (a) {
+  return a === 0;
+}
+
+/**
+ *
+ * @param {number} a
+ * @returns {boolean}
+ */
+function isOverZero (a) {
+  return a > 0;
+}
+
+/**
+ *
+ * @param {number} a
+ * @returns {boolean}
+ */
+function isBelowZero (a) {
+  return a < 0;
+}
+
+/**
+ *
+ * @param {number} a
+ * @returns {boolean}
+ */
+function isOverEqualZero (a) {
+  return a >= 0;
+}
+
+/**
+ *
+ * @param {number} a
+ * @returns {boolean}
+ */
+function isBelowEqualZero (a) {
+  return a <= 0;
+}
+
+/**
+ *
+ * @param {number} a
+ * @returns {boolean}
+ */
+function isNotZero (a) {
+  return a !== 0;
+}
+
+/**
+ *
+ * @param {string} a
+ * @returns {boolean}
+ */
+function isTest (a) {
+  return a === 'test';
+}
+
+/**
+ *
+ * @param {string} a
+ * @returns {boolean}
+ */
+function isStringOne (a) {
+  return a === '1';
+}
