da906dc5c8765f3549aafde7b31a7d0c3ef9b16c
Eugene Samoylenko <evgeniysamoylenko2000@gmail.com>
2020-06-25 17:14:46 +0300

    Add switch-case section

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // if-else
-console.log('if-else:');
+console.log('%c' + 'if-else:', 'font-weight: bold;');
 
 // 1.
+console.log('1.');
+
 console.log('isZero(1) = ', isZero(1));
 console.log('isZero(0) = ', isZero(0));
 console.log('isZero(-3) = ', isZero(-3));
@@ -18,6 +20,8 @@ function isZero (a) {
 }
 
 // 2.
+console.log('2.');
+
 console.log('isOverZero(1) = ', isOverZero(1));
 console.log('isOverZero(0) = ', isOverZero(0));
 console.log('isOverZero(-3) = ', isOverZero(-3));
@@ -34,6 +38,8 @@ function isOverZero (a) {
 }
 
 // 3.
+console.log('3.');
+
 console.log('isBelowZero(1) = ', isBelowZero(1));
 console.log('isBelowZero(0) = ', isBelowZero(0));
 console.log('isBelowZero(-3) = ', isBelowZero(-3));
@@ -50,6 +56,8 @@ function isBelowZero (a) {
 }
 
 // 4.
+console.log('4.');
+
 console.log('isOverEqualZero(1) = ', isOverEqualZero(1));
 console.log('isOverEqualZero(0) = ', isOverEqualZero(0));
 console.log('isOverEqualZero(-3) = ', isOverEqualZero(-3));
@@ -66,6 +74,8 @@ function isOverEqualZero (a) {
 }
 
 // 5.
+console.log('5.');
+
 console.log('isBelowEqualZero(1) = ', isBelowEqualZero(1));
 console.log('isBelowEqualZero(0) = ', isBelowEqualZero(0));
 console.log('isBelowEqualZero(-3) = ', isBelowEqualZero(-3));
@@ -82,6 +92,8 @@ function isBelowEqualZero (a) {
 }
 
 // 6.
+console.log('6.');
+
 console.log('isNotZero(1) = ', isNotZero(1));
 console.log('isNotZero(0) = ', isNotZero(0));
 console.log('isNotZero(-3) = ', isNotZero(-3));
@@ -98,6 +110,8 @@ function isNotZero (a) {
 }
 
 // 7.
+console.log('7.');
+
 console.log('isTest(1) = ', isTest('test'));
 console.log('isTest(0) = ', isTest('тест'));
 console.log('isTest(-3) = ', isTest(3));
@@ -114,6 +128,8 @@ function isTest (a) {
 }
 
 // 8.
+console.log('8.');
+
 console.log('isStringOne(1) = ', isStringOne('1'));
 console.log('isStringOne(0) = ', isStringOne(1));
 console.log('isStringOne(-3) = ', isStringOne(3));
@@ -130,8 +146,10 @@ function isStringOne (a) {
 }
 
 // Логические переменные
-console.log('Логические переменные:');
+console.log('%c' + 'Логические переменные:', 'font-weight: bold;');
 // 1.
+console.log('1.');
+
 let test = true;
 
 if (test) {
@@ -147,6 +165,9 @@ console.log(test ? 'Верно' : 'Неверно');
 console.log('');
 
 // 2.
+console.log('2.');
+
+
 test = true;
 
 if (!test) {
@@ -162,8 +183,10 @@ console.log(!test ? 'Верно' : 'Неверно');
 console.log('');
 
 // Работа с && , ||
-console.log('&&, ||:');
+console.log('%c' + '&&, ||:', 'font-weight: bold;');
 // 1.
+console.log('1.');
+
 console.log('isOver0Under5(5) = ', isOver0Under5(5));
 console.log('isOver0Under5(0) = ', isOver0Under5(0));
 console.log('isOver0Under5(-3) = ', isOver0Under5(-3));
@@ -181,6 +204,7 @@ function isOver0Under5 (a) {
 }
 
 // 2.
+console.log('2.');
 
 console.log('a === 0 || a === 2 ? a += 7 : a /= 10 , a=(5) = ', second(5));
 console.log('a === 0 || a === 2 ? a += 7 : a /= 10 , a=(0) = ', second(0));
@@ -199,6 +223,7 @@ function second (a) {
 }
 
 // 3.
+console.log('3.');
 
 console.log('a <= 1 && b >= 3 ? a + b : a - b , (1, 3) = ', third(1, 3));
 console.log('a <= 1 && b >= 3 ? a + b : a - b , (0, 6) = ', third(0, 6));
@@ -217,6 +242,7 @@ function third (a, b) {
 }
 
 // 4.
+console.log('4.');
 
 console.log('a > 2 && a < 11 || b >= 6 && b < 14 , (7, 6) = ', fourth(7, 6));
 console.log('a > 2 && a < 11 || b >= 6 && b < 14 , (1, 14) = ', fourth(1, 14));
@@ -232,3 +258,167 @@ console.log('');
 function fourth (a, b) {
   return a > 2 && a < 11 || b >= 6 && b < 14;
 }
+
+// switch-case
+console.log('%c' + 'switch-case:', 'font-weight: bold;');
+// 1.
+console.log('1.');
+
+const num = 1;
+let result;
+
+switch (num) {
+  case 1: {
+    result = 'Зима';
+  }
+    break;
+  case 2: {
+    result = 'Весна';
+  }
+    break;
+  case 3: {
+    result = 'Лето';
+  }
+    break;
+  case 4: {
+    result = 'Осень';
+  }
+    break;
+}
+
+console.log(`num(${num}) = `, result);
+console.log('');
+
+// 2.
+console.log('2.');
+
+const day = 24;
+let dec;
+let res2;
+
+if (day >= 1 && day <= 10) {
+  dec = 1;
+} else if (day >= 11 && day <= 20) {
+  dec = 2;
+} else {
+  dec = 3;
+}
+switch (dec) {
+  case 1: {
+    res2 = 'first decade';
+  }
+    break;
+  case 2: {
+    res2 = 'second decade';
+  }
+    break;
+  case 3: {
+    res2 = 'third decade';
+  }
+    break;
+}
+
+console.log(`day(${day}) = `, res2);
+console.log('');
+
+// 3.
+console.log('3.');
+
+const month = 6;
+let res3;
+
+switch (month) {
+  case 1:
+  case 2:
+  case 12: {
+    res3 = 'Зима';
+  }
+    break;
+  case 3:
+  case 4:
+  case 5: {
+    res3 = 'Весна';
+  }
+    break;
+  case 6:
+  case 7:
+  case 8: {
+    res3 = 'Лето';
+  }
+    break;
+  case 9:
+  case 10:
+  case 11: {
+    res3 = 'Осень';
+  }
+    break;
+}
+
+console.log(`month(${month}) = `, res3);
+console.log('');
+
+// 4.
+console.log('4.');
+
+const str1 = 'abcde';
+let res4;
+
+switch (str1[0]) {
+  case 'a': {
+    res4 = 'yes';
+  }
+    break;
+  default: {
+    res4 = 'no';
+  }
+    break;
+}
+
+console.log(`${str1} = `, res4);
+console.log('');
+
+// 5.
+console.log('5.');
+
+const str2 = '12345';
+let res5;
+
+switch (str2[0]) {
+  case '1':
+  case '2':
+  case '3': {
+    res5 = 'yes';
+  }
+    break;
+  default: {
+    res5 = 'no';
+  }
+    break;
+}
+
+console.log(`${str2} = `, res5);
+console.log('');
+
+// 6.
+console.log('6.');
+
+const str3 = '472';
+let res6;
+
+res6 = Number(str3[0]) + Number(str3[1]) + Number(str3[2]);
+
+console.log(`${str3}(${str3[0]} + ${str3[1]} + ${str3[2]}) = `, res6);
+console.log('');
+
+// 7.
+console.log('7.');
+
+const str4 = '438915';
+let res7 = 'no';
+
+if (+str4[0] + +str4[1] + +str4[2] === +str4[3] + +str4[4] + +str4[5]){
+  res7 = 'yes';
+}
+
+console.log(`${str4} = `, res7);
+console.log('');
