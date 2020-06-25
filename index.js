c4c3ac554fc5622d703418d4b72a4d5b5bc286b2
Eugene Samoylenko <evgeniysamoylenko2000@gmail.com>
2020-06-25 16:13:15 +0300

    Refactor code

--- index.js
+++ index.js
@@ -1,51 +1,10 @@
-// First part
-console.log('First part:')
+// if-else
+console.log('if-else:')
 
+// 1.
 console.log('isZero(1) = ', isZero(1));
 console.log('isZero(0) = ', isZero(0));
 console.log('isZero(-3) = ', isZero(-3));
-
-console.log('');
-
-console.log('isOverZero(1) = ', isOverZero(1));
-console.log('isOverZero(0) = ', isOverZero(0));
-console.log('isOverZero(-3) = ', isOverZero(-3));
-
-console.log('');
-
-console.log('isBelowZero(1) = ', isBelowZero(1));
-console.log('isBelowZero(0) = ', isBelowZero(0));
-console.log('isBelowZero(-3) = ', isBelowZero(-3));
-
-console.log('');
-
-console.log('isOverEqualZero(1) = ', isOverEqualZero(1));
-console.log('isOverEqualZero(0) = ', isOverEqualZero(0));
-console.log('isOverEqualZero(-3) = ', isOverEqualZero(-3));
-
-console.log('');
-
-console.log('isBelowEqualZero(1) = ', isBelowEqualZero(1));
-console.log('isBelowEqualZero(0) = ', isBelowEqualZero(0));
-console.log('isBelowEqualZero(-3) = ', isBelowEqualZero(-3));
-
-console.log('');
-
-console.log('isNotZero(1) = ', isNotZero(1));
-console.log('isNotZero(0) = ', isNotZero(0));
-console.log('isNotZero(-3) = ', isNotZero(-3));
-
-console.log('');
-
-console.log('isTest(1) = ', isTest('test'));
-console.log('isTest(0) = ', isTest('тест'));
-console.log('isTest(-3) = ', isTest(3));
-
-console.log('');
-
-console.log('isStringOne(1) = ', isStringOne('1'));
-console.log('isStringOne(0) = ', isStringOne(1));
-console.log('isStringOne(-3) = ', isStringOne(3));
 
 console.log('');
 
@@ -58,6 +17,13 @@ function isZero (a) {
   return a === 0;
 }
 
+// 2.
+console.log('isOverZero(1) = ', isOverZero(1));
+console.log('isOverZero(0) = ', isOverZero(0));
+console.log('isOverZero(-3) = ', isOverZero(-3));
+
+console.log('');
+
 /**
  *
  * @param {number} a
@@ -66,6 +32,13 @@ function isZero (a) {
 function isOverZero (a) {
   return a > 0;
 }
+
+// 3.
+console.log('isBelowZero(1) = ', isBelowZero(1));
+console.log('isBelowZero(0) = ', isBelowZero(0));
+console.log('isBelowZero(-3) = ', isBelowZero(-3));
+
+console.log('');
 
 /**
  *
@@ -76,6 +49,13 @@ function isBelowZero (a) {
   return a < 0;
 }
 
+// 4.
+console.log('isOverEqualZero(1) = ', isOverEqualZero(1));
+console.log('isOverEqualZero(0) = ', isOverEqualZero(0));
+console.log('isOverEqualZero(-3) = ', isOverEqualZero(-3));
+
+console.log('');
+
 /**
  *
  * @param {number} a
@@ -84,6 +64,13 @@ function isBelowZero (a) {
 function isOverEqualZero (a) {
   return a >= 0;
 }
+
+// 5.
+console.log('isBelowEqualZero(1) = ', isBelowEqualZero(1));
+console.log('isBelowEqualZero(0) = ', isBelowEqualZero(0));
+console.log('isBelowEqualZero(-3) = ', isBelowEqualZero(-3));
+
+console.log('');
 
 /**
  *
@@ -94,6 +81,13 @@ function isBelowEqualZero (a) {
   return a <= 0;
 }
 
+// 6.
+console.log('isNotZero(1) = ', isNotZero(1));
+console.log('isNotZero(0) = ', isNotZero(0));
+console.log('isNotZero(-3) = ', isNotZero(-3));
+
+console.log('');
+
 /**
  *
  * @param {number} a
@@ -103,6 +97,13 @@ function isNotZero (a) {
   return a !== 0;
 }
 
+// 7.
+console.log('isTest(1) = ', isTest('test'));
+console.log('isTest(0) = ', isTest('тест'));
+console.log('isTest(-3) = ', isTest(3));
+
+console.log('');
+
 /**
  *
  * @param {string} a
@@ -111,6 +112,13 @@ function isNotZero (a) {
 function isTest (a) {
   return a === 'test';
 }
+
+// 8.
+console.log('isStringOne(1) = ', isStringOne('1'));
+console.log('isStringOne(0) = ', isStringOne(1));
+console.log('isStringOne(-3) = ', isStringOne(3));
+
+console.log('');
 
 /**
  *
